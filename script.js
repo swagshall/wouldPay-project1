46c2b2d37a0cf37e18ee8eb60e6b9ea13b7d509c
ahmed <mueed@Usc.edu>
2022-02-02 14:03:49 -0500

    Proper statements to print data from Walmart Database' git push

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-// fetch statement for amazon API //
+// // fetch statement for amazon API //
 
-fetch("https://amazon-products1.p.rapidapi.com/search?country=US&query=MacBook%2BPro&page=1", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "amazon-products1.p.rapidapi.com",
-		"x-rapidapi-key": "47388dc074msh8f2434201e3f0d0p12cfd8jsnd9b2a609b60a"
-	}
-})
-.then(function (response){
-	return response.json();
-})
-.then (function (data){
-	console.log(data);
-});
+// var amazonURL = 'https://amazon-products1.p.rapidapi.com/search?country=US&query=MacBook%2BPro&page=1';
+
+// fetch(amazonURL, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "amazon-products1.p.rapidapi.com",
+// 		"x-rapidapi-key": "47388dc074msh8f2434201e3f0d0p12cfd8jsnd9b2a609b60a"
+// 	}
+// })
+// .then(function (response){
+// 	return response.json();
+// })
+// .then (function (data){
+// 	console.log(data);
+// });
 
 
 
-// fetch statemtn for Walmart API //
+// fetch statement for Walmart API //
 
-fetch("https://walmart2.p.rapidapi.com/search?query=xiaomi", {
+var walmartURL = 'https://walmart2.p.rapidapi.com/search?query=airpods+pro';
+
+fetch(walmartURL, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "walmart2.p.rapidapi.com",
 		"x-rapidapi-key": "42cf103a4fmsh68f4de9dafb003cp1f5e10jsn97190e2b7d10"
 	}
 })
+
 .then(function (response){
 	return response.json();
 })
 .then (function (data){
 	console.log(data);
+    console.log(data.items[0]);
+    $('.card-text').text(data.items[0].brand);
+    $('.card-text').text(data.items[0].description)
+
 });
+
+
+
 
