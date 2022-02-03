0d163567ccb2f31886ed60574c157d707cc283a3
ahmed <mueed@Usc.edu>
2022-02-03 13:45:12 -0500

    Added info for Amazaon Search

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // // fetch statement for amazon API //
 var productContainer = document.querySelector("#card-container");
 var productSelect = document.querySelector("#product");
+
 var submitBtn = document.querySelector("#submitBtn");
 var userOutput = [];
 
@@ -17,6 +18,7 @@ var walmartReviews = document.querySelector('#amazonReviews');
 var walmartStars = document.querySelector('#amazonStars');
 var wlalmartmage = document.querySelector('#amazonImage');
 
+
 console.log(productContainer);
 console.log(productSelect);
 console.log(amazonName);
@@ -30,7 +32,7 @@ var amazonFetch = function(event){
 	event.preventDefault();
 	console.log('hello')
 
-var amazonURL = 'https://amazon-products1.p.rapidapi.com/search?country=US&query=MacBook%2BPro&page=1';
+var amazonURL = 'https://amazon-products1.p.rapidapi.com/search?country=US&query=samsung';
 
 fetch(amazonURL, {
 	"method": "GET",
@@ -45,17 +47,22 @@ fetch(amazonURL, {
 .then (function (data){
 	console.log(data);
 amazonName.textContent = data.results[0].title;
+amazonReviews.textContent = data.results[0].reviews.total_reviews;
+amazonStars.textContent= data.results[0].reviews.stars;
+amazonImage.setAttribute('src', data.results[0].image);
+
 
 });
 
 }
 
-var displayAmazonFetch = function(amazonQuery){
-	productContainer.textContent = "";
-	var productName = document.createElement("span");
-	productName.textContent = $('.card-text').text(data.results[0].title);
+// var displayAmazonFetch = function(amazonQuery){
+// 	productContainer.textContent = "";
+// 	var productName = document.createElement("span");
+// 	productName.textContent = $('.card-text').text(data.results[0].title);
+
 	
-}
+// }
 
 
 var submitHandler = function (event){
@@ -79,34 +86,35 @@ submitBtn.addEventListener('click', amazonFetch);
 
 
 
+
 // fetch statement for Walmart API //
-      // Searching for AirPod Pros
+//       Searching for AirPod Pros
 
-// var walmartURL = 'https://walmart2.p.rapidapi.com/search?query=airpods+pro';
+var walmartURL = 'https://walmart2.p.rapidapi.com/search?query=airpods+pro';
 
-// fetch(walmartURL, {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "walmart2.p.rapidapi.com",
-// 		"x-rapidapi-key": "42cf103a4fmsh68f4de9dafb003cp1f5e10jsn97190e2b7d10"
-// 	}
-// })
-// .then(function (response){
-// 	return response.json();
-// })
-// .then (function (data){
+fetch(walmartURL, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "walmart2.p.rapidapi.com",
+		"x-rapidapi-key": "42cf103a4fmsh68f4de9dafb003cp1f5e10jsn97190e2b7d10"
+	}
+})
+.then(function (response){
+	return response.json();
+})
+.then (function (data){
 
-// 	console.log(data);
-//      console.log(data.items[0])  
+	console.log(data);
+     console.log(data.items[0])  
 
-// 	 $('.card-text').text(data.items[0].custom);
-// 	 $('#product').text(data.items[0].brand);
+	 $('.card-text').text(data.items[0].custom);
+	 $('#product').text(data.items[0].brand);
 
 
-// /// All of the DOM manipul.atiion is going to happen in here //
+/// All of the DOM manipul.atiion is going to happen in here //
 
-//     $('#descriptiion').text(data.items[0].description);
+    $('#descriptiion').text(data.items[0].description);
 
  
-// });
+});
 
