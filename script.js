2df80d206540a30efad2a573f2ab9798c87f9f91
ahmed <mueed@Usc.edu>
2022-02-03 20:26:23 -0500

    got both sides of the search to function

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ console.log(content);
 console.log(header);
 
 //Selector Statements for Side Bar - Amazon //
-var amazonSearch = document.querySelector("#amazonProduct");
+var amazonSearchArea = document.querySelector("#amazonProduct");
 var amazonSubmit = document.querySelector('#submitBtn1');
 
 //Selector Statements for Side Bar - Walmart //
-var walmartSearch = document.querySelector("#walmartProduct");
+var walmartSearchArea = document.querySelector("#walmartProduct");
 var walmartSubmit = document.querySelector('#submitBtn2');
 
-console.log(walmartSearch, walmartSubmit, amazonSearch, amazonSubmit);
-
+console.log(walmartSubmit, amazonSubmit);
 
 // Amazon Card Query Selectors //
 var amazonName = document.querySelector('#amazonName');
@@ -24,7 +23,6 @@ var amazonStars = document.querySelector('#amazonStars');
 var amazonImage = document.querySelector('#amazonImage');
 
 console.log(amazonName,amazonReviews,amazonStars,amazonImage);
-
 
 // Walmart Card Query Selectors //
 var walmartName = document.querySelector('#walmartName');
@@ -36,28 +34,33 @@ console.log(walmartName,walmartReviews,walmartStars,walmartImage);
 
 // End of Query Selectors//
 
-// Adding Event Handlers to Submit Buttions  //
-
 amazonSubmit.addEventListener('click', amazonFetch);
-walmartSubmit.addEventListener('click', walmartFetch);
+walmartSubmit.addEventListener ('click', walmartFetch);
 
 
-// Fetch Statements  //
 
-// Amazon Fetch Function // 
-//varible for userinput for search
-var userInputAmazon = ""
+
+// // Fetch Statements  //
+
+// // Amazon Fetch Function // 
+
+
 
 function amazonFetch (event){
+
+let amazonSearch = amazonSearchArea.value;
+var amazonUrl = 'https://amazon-products1.p.rapidapi.com/search?country=US&query=' + amazonSearch + '&page=1';
+
+
 	event.preventDefault();
 	console.log('amazon');
-	fetch("https://amazon-products1.p.rapidapi.com/search?country=US&query=" + userInputAmazon, {
-		"method": "GET",
-		"headers": {
+fetch(amazonUrl , {
+	"method": "GET",
+	"headers": {
 			"x-rapidapi-host": "amazon-products1.p.rapidapi.com",
-			"x-rapidapi-key": "9473a74dfamshd8d87205311e913p1a7df2jsneb5c88b554ea"
-		}
-	})
+		"x-rapidapi-key": "af077beecbmsha635137a0c28f34p15546fjsn39f9feb23650"
+	}
+})
 .then(function (response){
 	return response.json();
 })
@@ -71,25 +74,28 @@ amazonImage.setAttribute('src', data.results[0].image);
 });
 }
 
-// Walmart Fetch Function //
-//userInput Walmart
-var userInputWalmart = "Macbook"
+// // Walmart Fetch Function //
+
 function walmartFetch(event) {
 	event.preventDefault();
 	console.log('walmart');
 
-	fetch("https://walmart2.p.rapidapi.com/search?query=" + userInputWalmart, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "walmart2.p.rapidapi.com",
-			"x-rapidapi-key": "9473a74dfamshd8d87205311e913p1a7df2jsneb5c88b554ea"
-		}
-	})
-	.then(function (response){
+let walmartSearch = walmartSearchArea.value;	
+var walmartUrl = 'https://walmart2.p.rapidapi.com/search?query=' + walmartSearch;
+
+fetch(walmartUrl, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "walmart2.p.rapidapi.com",
+		"x-rapidapi-key": "9473a74dfamshd8d87205311e913p1a7df2jsneb5c88b554ea"
+	}
+})
+.then(function (response){
 		return response.json();
 	})
 	.then (function (data){
 		console.log(data);
+		console.log('hello motto')
 walmartName.textContent = data.items[0].title;
 walmartReviews.textContent = data.items[0].numReviews;
 walmartStars.textContent= data.items[0].customerRating;
@@ -109,42 +115,42 @@ walmartImage.setAttribute('src', data.items[0].imageUrl);
 
 
 
-// // var displayAmazonFetch = function(amazonQuery){
-// // 	productContainer.textContent = "";
-// // 	var productName = document.createElement("span");
-// // 	productName.textContent = $('.card-text').text(data.results[0].title);
+// // // var displayAmazonFetch = function(amazonQuery){
+// // // 	productContainer.textContent = "";
+// // // 	var productName = document.createElement("span");
+// // // 	productName.textContent = $('.card-text').text(data.results[0].title);
 
 	
+// // // }
+
+
+// // var submitHandler = function (event){
+// // 	event.preventDefault();
+// // 	var userInput = amazonQuery.ariaValueMax.trim();
+// // 	if (userInput){
+// // 		amazonFetch(userInput);
+// // 		userOutput.unshift({userInput});
+// // 		userInput.value = "";
+// // 	} else {
+// // 		alert("Please enter a product name");
+// // 	}
+// // 	saveInput();
 // // }
 
-
-// var submitHandler = function (event){
-// 	event.preventDefault();
-// 	var userInput = amazonQuery.ariaValueMax.trim();
-// 	if (userInput){
-// 		amazonFetch(userInput);
-// 		userOutput.unshift({userInput});
-// 		userInput.value = "";
-// 	} else {
-// 		alert("Please enter a product name");
-// 	}
-// 	saveInput();
-// }
-
-// var saveInput= function(){
-// 	localStorage.setItem("userOutput", JSON.stringify(userOUtput))
-// };
+// // var saveInput= function(){
+// // 	localStorage.setItem("userOutput", JSON.stringify(userOUtput))
+// // };
 
 
 
 
 
-// // fetch statement for Walmart API //
-// //       Searching for AirPod Pros
+// // // fetch statement for Walmart API //
+// // //       Searching for AirPod Pros
 
 
 
 
 
-var userOutput = [];
-console.log(userOutput);
+// var userOutput = [];
+// console.log(userOutput);
